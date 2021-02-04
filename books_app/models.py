49714eb61174e987376c8744c144ee3cd883f730
Starlight Romero <starlightromero@protonmail.com>
2021-02-04 15:32:16 -0800

    [update] added new field to be received from form

--- books_app/models.py
+++ books_app/models.py
@@ -3,8 +3,10 @@ from books_app import db
 from sqlalchemy.orm import backref
 import enum
 
+
 class FormEnum(enum.Enum):
     """Helper class to make it easier to use enums with forms."""
+
     @classmethod
     def choices(cls):
         return [(choice.name, choice) for choice in cls]
@@ -12,77 +14,91 @@ class FormEnum(enum.Enum):
     def __str__(self):
         return str(self.value)
 
+
 class Audience(FormEnum):
-    CHILDREN = 'Children'
-    YOUNG_ADULT = 'Young Adult'
-    ADULT = 'Adult'
-    ALL = 'All'
+    CHILDREN = "Children"
+    YOUNG_ADULT = "Young Adult"
+    ADULT = "Adult"
+    ALL = "All"
+
 
 class Book(db.Model):
     """Book model."""
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80), nullable=False)
     publish_date = db.Column(db.Date)
 
     # The author - Who wrote it?
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
-    author = db.relationship('Author', back_populates='books')
-    
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"), nullable=False)
+    author = db.relationship("Author", back_populates="books")
+
     # The audience - Who is this book written for?
     audience = db.Column(db.Enum(Audience), default=Audience.ALL)
 
     # The genres, e.g. fiction, sci-fi, fantasy
-    genres = db.relationship(
-        'Genre', secondary='book_genre', back_populates='books')
+    genres = db.relationship("Genre", secondary="book_genre", back_populates="books")
 
     # Who favorited this book?
     users_who_favorited = db.relationship(
-        'User', secondary='user_book', back_populates='favorite_books')
+        "User", secondary="user_book", back_populates="favorite_books"
+    )
 
     def __str__(self):
-        return f'<Book: {self.title}>'
+        return f"<Book: {self.title}>"
 
     def __repr__(self):
-        return f'<Book: {self.title}>'
+        return f"<Book: {self.title}>"
+
 
 class Author(db.Model):
     """Author model."""
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    biography = db.Column(db.String(200))
-    books = db.relationship('Book', back_populates='author')
+    biography = db.Column(db.String(200), nullable=False)
+    dob = db.Column(db.DateTime, nullable=False)
+    country = db.Column(db.String(30), nullable=False)
+    books = db.relationship("Book", back_populates="author")
 
     def __str__(self):
-        return f'<Author: {self.name}>'
+        return f"<Author: {self.name}>"
 
     def __repr__(self):
-        return f'<Author: {self.name}>'
+        return f"<Author: {self.name}>"
+
 
 class Genre(db.Model):
     """Genre model."""
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=True)
-    books = db.relationship(
-        'Book', secondary='book_genre', back_populates='genres')
+    books = db.relationship("Book", secondary="book_genre", back_populates="genres")
 
     def __str__(self):
-        return f'<Genre: {self.name}>'
+        return f"<Genre: {self.name}>"
 
     def __repr__(self):
-        return f'<Genre: {self.name}>'
+        return f"<Genre: {self.name}>"
 
-book_genre_table = db.Table('book_genre',
-    db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
-    db.Column('genre_id', db.Integer, db.ForeignKey('genre.id'))
+
+book_genre_table = db.Table(
+    "book_genre",
+    db.Column("book_id", db.Integer, db.ForeignKey("book.id")),
+    db.Column("genre_id", db.Integer, db.ForeignKey("genre.id")),
 )
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     favorite_books = db.relationship(
-        'Book', secondary='user_book', back_populates='users_who_favorited')
+        "Book", secondary="user_book", back_populates="users_who_favorited"
+    )
 
-favorite_books_table = db.Table('user_book',
-    db.Column('book_id', db.Integer, db.ForeignKey('book.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
+
+favorite_books_table = db.Table(
+    "user_book",
+    db.Column("book_id", db.Integer, db.ForeignKey("book.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
 )
