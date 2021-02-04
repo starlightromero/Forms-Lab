36b8522b7a4bf67868da52dccb6706dd1a993f71
Starlight Romero <starlightromero@protonmail.com>
2021-02-04 15:32:42 -0800

    [update] finished TODOs for create_author

--- books_app/routes.py
+++ books_app/routes.py
@@ -4,8 +4,6 @@ from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
 from books_app.models import Book, Author, Genre, User
 from books_app.forms import BookForm, AuthorForm, GenreForm
-
-# Import app and db from events_app package so that we can run app
 from books_app import app, db
 
 main = Blueprint("main", __name__)
@@ -14,46 +12,50 @@ main = Blueprint("main", __name__)
 #           Routes                       #
 ##########################################
 
-@main.route('/')
+
+@main.route("/")
 def homepage():
     all_books = Book.query.all()
     all_users = User.query.all()
-    return render_template('home.html', 
-        all_books=all_books, all_users=all_users)
+    return render_template("home.html", all_books=all_books, all_users=all_users)
 
-@main.route('/create_book', methods=['GET', 'POST'])
+
+@main.route("/create_book", methods=["GET", "POST"])
 def create_book():
     form = BookForm()
-
-    # if form was submitted and contained no errors
-    if form.validate_on_submit(): 
+    if form.validate_on_submit():
         new_book = Book(
             title=form.title.data,
             publish_date=form.publish_date.data,
             author=form.author.data,
             audience=form.audience.data,
-            genres=form.genres.data
+            genres=form.genres.data,
         )
         db.session.add(new_book)
         db.session.commit()
+        flash("New book was created successfully.")
+        return redirect(url_for("main.book_detail", book_id=new_book.id))
+    return render_template("create_book.html", form=form)
 
-        flash('New book was created successfully.')
-        return redirect(url_for('main.book_detail', book_id=new_book.id))
-    return render_template('create_book.html', form=form)
 
-@main.route('/create_author', methods=['GET', 'POST'])
+@main.route("/create_author", methods=["GET", "POST"])
 def create_author():
-    # TODO: Make an AuthorForm instance
+    form = AuthorForm()
+    if form.validate_on_submit():
+        new_author = Author(
+            name=form.name.data,
+            biography=form.biography.data,
+            dob=form.dob.data,
+            country=form.country.data,
+        )
+        db.session.add(new_author)
+        db.session.commit()
+        flash("New author was created successfully.")
+        return redirect(url_for("main.homepage"))
+    return render_template("create_author.html", form=form)
 
-    # TODO: If the form was submitted and is valid, create a new Author object
-    # and save to the database, then flash a success message to the user and
-    # redirect to the homepage
 
-    # TODO: Send the form object to the template, and use it to render the form
-    # fields
-    return render_template('create_author.html')
-
-@main.route('/create_genre', methods=['GET', 'POST'])
+@main.route("/create_genre", methods=["GET", "POST"])
 def create_genre():
     # TODO: Make a GenreForm instance
 
@@ -63,29 +65,32 @@ def create_genre():
 
     # TODO: Send the form object to the template, and use it to render the form
     # fields
-    return render_template('create_genre.html')
+    return render_template("create_genre.html")
 
-@main.route('/create_user', methods=['GET', 'POST'])
+
+@main.route("/create_user", methods=["GET", "POST"])
 def create_user():
     # STRETCH CHALLENGE: Fill out the Create User route
     return "Not Yet Implemented"
 
-@main.route('/book/<book_id>', methods=['GET', 'POST'])
+
+@main.route("/book/<book_id>", methods=["GET", "POST"])
 def book_detail(book_id):
     book = Book.query.get(book_id)
     form = BookForm(obj=book)
 
-    # TODO: If the form was submitted and is valid, update the fields in the 
-    # Book object and save to the database, then flash a success message to the 
+    # TODO: If the form was submitted and is valid, update the fields in the
+    # Book object and save to the database, then flash a success message to the
     # user and redirect to the book detail page
 
-    return render_template('book_detail.html', book=book, form=form)
+    return render_template("book_detail.html", book=book, form=form)
 
-@main.route('/profile/<username>')
+
+@main.route("/profile/<username>")
 def profile(username):
     # TODO: Make a query for the user with the given username, and send to the
     # template
 
-    # STRETCH CHALLENGE: Add ability to modify a user's username or favorite 
+    # STRETCH CHALLENGE: Add ability to modify a user's username or favorite
     # books
-    return render_template('profile.html', username=username)
+    return render_template("profile.html", username=username)
